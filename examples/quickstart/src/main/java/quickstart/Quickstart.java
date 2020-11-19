399205a0820139b9d6429f90ca04ed54228b2a89
Arvind Krishnakumar <61501885+arvindkrishnakumar-okta@users.noreply.github.com>
2020-11-19 08:48:49 -0800

    developmental commit

--- examples/quickstart/src/main/java/quickstart/Quickstart.java
+++ examples/quickstart/src/main/java/quickstart/Quickstart.java
@@ -15,20 +15,8 @@
  */
 package quickstart;
 
-import com.okta.sdk.api.client.Clients;
-import com.okta.sdk.api.client.OktaIdentityEngineClient;
-import com.okta.sdk.api.exception.ProcessingException;
-import com.okta.sdk.api.model.Credentials;
-import com.okta.sdk.api.model.Token;
-import com.okta.sdk.api.request.IdentifyRequest;
-import com.okta.sdk.api.response.OktaIdentityEngineResponse;
-import com.okta.sdk.api.exception.ProcessingException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
 
 /**
  * This class demonstrates the code found in the SDK QuickStart Guide
@@ -38,30 +26,9 @@ import java.util.Set;
 @SuppressWarnings("PMD.UnusedLocalVariable")
 public class Quickstart {
 
-    public static void main(String[] args) throws ProcessingException {
-        Set<String> scope = new HashSet<>(Arrays.asList("openid", "profile"));
+    private static final Logger log = LoggerFactory.getLogger(Quickstart.class);
 
-        OktaIdentityEngineClient client = Clients.builder()
-            .setIssuer("https://idx-devex.trexcloud.com")
-            .setClientId("0oa3jxy2kpqZs9fOU0g7")
-            .setClientSecret("6NMR5HuSlZ8LOM5X7jHqE9Up9xLOqoHA7NGymjPo")
-            .setScopes(scope)
-            .build();
-
-        OktaIdentityEngineResponse oktaIdentityEngineResponse = client.start();
-
-        String stateHandle = oktaIdentityEngineResponse.getStateHandle();
-
-        oktaIdentityEngineResponse = client.identify(new IdentifyRequest("arvind.krishnakumar@okta.com", new Credentials("Sclass15683!", null), false, stateHandle));
-
-        if (oktaIdentityEngineResponse.loginSuccess()) {
-            Token token = oktaIdentityEngineResponse.getSuccessWithInteractionCode().exchangeCode(client);
-            println(token.getAccessToken());
-        }
-    }
-
-    private static void println(String message) {
-        System.out.println(message);
-        System.out.flush();
+    public static void main(String[] args) {
+        //TODO
     }
 }
