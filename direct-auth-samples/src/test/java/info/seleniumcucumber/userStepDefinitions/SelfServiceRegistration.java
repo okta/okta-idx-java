3eac4acbb4c3bee812c43222fb8bb98a4d3141ae
Sergii Shamrai <71881713+sergiishamrai-okta@users.noreply.github.com>
2021-05-25 13:56:35 +0300

    self service registration: scenario 4.1.3

--- direct-auth-samples/src/test/java/info/seleniumcucumber/userStepDefinitions/SelfServiceRegistration.java
+++ direct-auth-samples/src/test/java/info/seleniumcucumber/userStepDefinitions/SelfServiceRegistration.java
@@ -4,41 +4,50 @@ import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class SelfServiceRegistration extends BasicDefinitions {
 
-    @Then("she sees a page to input her code1")
-    public void sheSeesAPageToInputHerCode1() {
-        sleep();
-        driver.findElement(By.id("proceed-btn")).click();
-    }
-
     @Given("Mary navigates to the Self Service Registration View")
     public void maryNavigatesToTheSelfServiceRegistrationView() {
+        driver.manage().window().maximize();
+        driver.get("http://localhost:8080");
+        driver.findElement(By.id("register")).click();
     }
 
     @When("she fills out her First Name")
     public void sheFillsOutHerFirstName() {
+        sleep();
+        driver.findElement(By.name("firstname")).sendKeys("Mary");
     }
 
     @And("she fills out her Last Name")
     public void sheFillsOutHerLastName() {
+        driver.findElement(By.name("lastname")).sendKeys("LastName");
     }
 
-    @And("she fills out her Email")
-    public void sheFillsOutHerEmail() {
-    }
-
-    @And("she fills out her Password")
-    public void sheFillsOutHerPassword() {
-    }
-
-    @And("she confirms her Password")
-    public void sheConfirmsHerPassword() {
+    @And("she fills out her Email with an invalid email format")
+    public void sheFillsOutHerEmailWithAnInvalidEmailFormat() {
+        driver.findElement(By.name("email")).sendKeys("invalid@acme");
     }
 
     @And("she submits the registration form")
     public void sheSubmitsTheRegistrationForm() {
+        driver.findElement(By.id("sign-in-btn")).click();
+    }
+
+    @Then("she sees an error message \"'Email' must be in the form of an email address, Provided value for property 'Email' does not match required pattern\"")
+    public void sheSeesAnErrorMessage() {
+        sleep();
+        By selection = By.className("alert-danger");
+        (new WebDriverWait(driver, 30)).until(
+                ExpectedConditions.visibilityOfElementLocated(selection));
+        String error = driver.findElement(By.className("alert-danger")).getText();
+        Assert.assertFalse("Error is not shown", error.isEmpty());
+        Assert.assertTrue("'Email' does not match required pattern error is shown",
+                error.contains("['Email' must be in the form of an email address, Provided value for property 'Email' does not match required pattern]"));
     }
 }
--- direct-auth-samples/src/test/resources/features/self_service_registration.feature
+++ direct-auth-samples/src/test/resources/features/self_service_registration.feature
@@ -5,9 +5,7 @@ Feature: Self Service Registration
     Given Mary navigates to the Self Service Registration View
     When she fills out her First Name
     And she fills out her Last Name
-    And she fills out her Email
-    And she fills out her Password
-    And she confirms her Password
+    And she fills out her Email with an invalid email format
     And she submits the registration form
     Then she sees an error message "'Email' must be in the form of an email address, Provided value for property 'Email' does not match required pattern"
 
