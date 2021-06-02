f7d6f9c789895ce936171d4687968d6f3b83deb6
sergiishamrai-okta <71881713+sergiishamrai-okta@users.noreply.github.com>
2021-06-02 16:40:23 +0300

    UTs for Scenarios 6.2.3 and 6.2.4 (#139)
    * UT for Scenario 6.2.3 and 6.2.4

--- api/src/test/groovy/com/okta/idx/sdk/api/client/IDXAuthenticationWrapperTest.groovy
+++ api/src/test/groovy/com/okta/idx/sdk/api/client/IDXAuthenticationWrapperTest.groovy
@@ -1049,6 +1049,125 @@ class IDXAuthenticationWrapperTest {
         assertThat(authenticationResponse.getTokenResponse().getIdToken(), notNullValue())
     }
 
+    @Test(testName = "Enroll with Invalid Phone Number")
+    void testMFAWithInvalidPhoneNumber() {
+
+        def scenario = "scenario_6_2_3"
+        def requestExecutor = mock(RequestExecutor)
+        def idxClient = new BaseIDXClient(getClientConfiguration(), requestExecutor)
+        def idxAuthenticationWrapper = new IDXAuthenticationWrapper()
+        //replace idxClient with mock idxClient
+        setInternalState(idxAuthenticationWrapper, "client", idxClient)
+
+        setMockResponse(requestExecutor, "interact", scenario + "/interact-response", 200, MediaType.APPLICATION_JSON)
+        setMockResponse(requestExecutor, "introspect", scenario + "/introspect-response", 200, mediaTypeAppIonJson)
+        setMockResponse(requestExecutor, "identify", scenario + "/identify-response", 200, mediaTypeAppIonJson)
+
+        AuthenticationResponse beginResponse = idxAuthenticationWrapper.begin()
+        AuthenticationResponse authenticationResponse = idxAuthenticationWrapper.authenticate(
+                new AuthenticationOptions("username", "password"), beginResponse.proceedContext
+        )
+        assertThat(authenticationResponse, notNullValue())
+        assertThat(authenticationResponse.getErrors(), empty())
+        assertThat(authenticationResponse.getAuthenticationStatus(),
+                is(AuthenticationStatus.AWAITING_AUTHENTICATOR_ENROLLMENT_SELECTION)
+        )
+        assertThat(authenticationResponse.getAuthenticators(), notNullValue())
+        assertThat(authenticationResponse.getAuthenticators(),
+                hasItem(hasProperty("label", is("Phone")))
+        )
+
+        Optional<Authenticator> authenticator = authenticationResponse.getAuthenticators()
+                .stream().filter({ auth -> auth.label.equals("Phone") }).findFirst()
+        assertThat("No Phone authenticator found", authenticator.isPresent())
+        setMockResponse(requestExecutor, "credential/enroll", scenario + "/challenge-response", 200, mediaTypeAppIonJson)
+        authenticationResponse = idxAuthenticationWrapper.selectAuthenticator(
+                authenticationResponse.proceedContext, authenticator.get()
+        )
+        assertThat(authenticationResponse, notNullValue())
+        assertThat(authenticationResponse.getAuthenticationStatus(),
+                is(AuthenticationStatus.AWAITING_AUTHENTICATOR_ENROLLMENT_DATA)
+        )
+
+        Optional<Authenticator.Factor> factor = authenticator.get().getFactors()
+                .stream().filter({ factor -> factor.label.equals("SMS") }).findFirst()
+        assertThat("No SMS factor found", factor.isPresent())
+        setMockResponse(requestExecutor, "credential/enroll", scenario + "/enroll-invalid-response", 400, mediaTypeAppIonJson)
+        authenticationResponse = idxAuthenticationWrapper.submitPhoneAuthenticator(
+                authenticationResponse.proceedContext, "+333333333333", factor.get()
+        )
+        assertThat(authenticationResponse, notNullValue())
+        assertThat(authenticationResponse.getErrors(), notNullValue())
+        assertThat(authenticationResponse.getErrors(), hasItem(
+                "Unable to initiate factor enrollment: Invalid Phone Number."
+        ))
+    }
+
+    @Test(testName = "User enters a wrong verification code on verify")
+    void testMFAWithInvalidCodeFromSMS() {
+
+        def scenario = "scenario_6_2_4"
+        def requestExecutor = mock(RequestExecutor)
+        def idxClient = new BaseIDXClient(getClientConfiguration(), requestExecutor)
+        def idxAuthenticationWrapper = new IDXAuthenticationWrapper()
+        //replace idxClient with mock idxClient
+        setInternalState(idxAuthenticationWrapper, "client", idxClient)
+
+        setMockResponse(requestExecutor, "interact", scenario + "/interact-response", 200, MediaType.APPLICATION_JSON)
+        setMockResponse(requestExecutor, "introspect", scenario + "/introspect-response", 200, mediaTypeAppIonJson)
+        setMockResponse(requestExecutor, "identify", scenario + "/identify-response", 200, mediaTypeAppIonJson)
+
+        AuthenticationResponse beginResponse = idxAuthenticationWrapper.begin()
+        AuthenticationResponse authenticationResponse = idxAuthenticationWrapper.authenticate(
+                new AuthenticationOptions("username", "password"), beginResponse.proceedContext
+        )
+        assertThat(authenticationResponse, notNullValue())
+        assertThat(authenticationResponse.getErrors(), empty())
+        assertThat(authenticationResponse.getAuthenticationStatus(),
+                is(AuthenticationStatus.AWAITING_AUTHENTICATOR_ENROLLMENT_SELECTION)
+        )
+        assertThat(authenticationResponse.getAuthenticators(), notNullValue())
+        assertThat(authenticationResponse.getAuthenticators(),
+                hasItem(hasProperty("label", is("Phone")))
+        )
+
+        Optional<Authenticator> authenticator = authenticationResponse.getAuthenticators()
+                .stream().filter({ auth -> auth.label.equals("Phone") }).findFirst()
+        assertThat("No Phone authenticator found", authenticator.isPresent())
+        setMockResponse(requestExecutor, "credential/enroll", scenario + "/challenge-response", 200, mediaTypeAppIonJson)
+        authenticationResponse = idxAuthenticationWrapper.selectAuthenticator(
+                authenticationResponse.proceedContext, authenticator.get()
+        )
+        assertThat(authenticationResponse, notNullValue())
+        assertThat(authenticationResponse.getAuthenticationStatus(),
+                is(AuthenticationStatus.AWAITING_AUTHENTICATOR_ENROLLMENT_DATA)
+        )
+
+        Optional<Authenticator.Factor> factor = authenticator.get().getFactors()
+                .stream().filter({ factor -> factor.label.equals("SMS") }).findFirst()
+        assertThat("No SMS factor found", factor.isPresent())
+        setMockResponse(requestExecutor, "credential/enroll", scenario + "/enroll-response", 200, mediaTypeAppIonJson)
+        authenticationResponse = idxAuthenticationWrapper.submitPhoneAuthenticator(
+                authenticationResponse.proceedContext, "+11234567890", factor.get()
+        )
+        assertThat(authenticationResponse, notNullValue())
+        assertThat(authenticationResponse.getAuthenticationStatus(),
+                is(AuthenticationStatus.AWAITING_AUTHENTICATOR_ENROLLMENT)
+        )
+
+        setMockResponse(requestExecutor, "challenge/answer", scenario + "/answer-challenge-sms-invalid-response", 401, mediaTypeAppIonJson)
+        VerifyAuthenticatorOptions verifyAuthenticatorOptions = new VerifyAuthenticatorOptions("123456")
+        authenticationResponse = idxAuthenticationWrapper.verifyAuthenticator(
+                authenticationResponse.proceedContext, verifyAuthenticatorOptions
+        )
+
+        assertThat(authenticationResponse, notNullValue())
+        assertThat(authenticationResponse.getErrors(), notNullValue())
+        assertThat(authenticationResponse.getErrors(), hasItem(
+                "Invalid code. Try again."
+        ))
+    }
+
     void setMockResponse(RequestExecutor requestExecutor, String resourceUrlEndsWith,
                          String responseName, Integer httpStatus, MediaType mediaType) {
         when(requestExecutor.executeRequest(
