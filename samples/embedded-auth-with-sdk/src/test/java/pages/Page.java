2f0fb5e399524e5bdd57bc2998ff5d26c7ac1700
Vitalii Tytarenko <Vitalii.Tytarenko@okta.com>
2021-06-18 02:13:46 +0300

    Increase the number of attempts and timeout

--- samples/embedded-auth-with-sdk/src/test/java/pages/Page.java
+++ samples/embedded-auth-with-sdk/src/test/java/pages/Page.java
@@ -30,7 +30,8 @@ import java.util.regex.Pattern;
 
 public class Page {
 
-    private static final int RETRY_COUNT = 30;
+    private static final int RETRY_COUNT = 40;
+    private static final int TIME_OUT_IN_SECONDS = 20;
 
     protected WebDriver driver;
 
@@ -72,7 +73,7 @@ public class Page {
     }
 
     public void waitForWebElementDisplayed(WebElement webElement) {
-        new WebDriverWait(driver, 10)
+        new WebDriverWait(driver, TIME_OUT_IN_SECONDS)
                 .until(ExpectedConditions.visibilityOf(webElement));
     }
 
