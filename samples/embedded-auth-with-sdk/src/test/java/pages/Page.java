2849e6f5e1ead472ad2208c140feb6c30d545228
Vitalii Tytarenko <Vitalii.Tytarenko@okta.com>
2021-06-17 22:30:33 +0300

    Increase the number of attempts

--- samples/embedded-auth-with-sdk/src/test/java/pages/Page.java
+++ samples/embedded-auth-with-sdk/src/test/java/pages/Page.java
@@ -30,7 +30,7 @@ import java.util.regex.Pattern;
 
 public class Page {
 
-    private static final int RETRY_COUNT = 10;
+    private static final int RETRY_COUNT = 15;
 
     protected WebDriver driver;
 
