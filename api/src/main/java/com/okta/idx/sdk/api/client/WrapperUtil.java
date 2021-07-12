07686b8e47dc4a682bd002c27ba39e4a22301bc2
sergiishamrai-okta <71881713+sergiishamrai-okta@users.noreply.github.com>
2021-07-12 18:08:51 +0300

    Log spam (#220)

--- api/src/main/java/com/okta/idx/sdk/api/client/WrapperUtil.java
+++ api/src/main/java/com/okta/idx/sdk/api/client/WrapperUtil.java
@@ -65,11 +65,11 @@ final class WrapperUtil {
     static void printRemediationOptions(IDXResponse idxResponse) {
         if (idxResponse != null && idxResponse.remediation() != null) {
             RemediationOption[] remediationOptions = idxResponse.remediation().remediationOptions();
-            logger.info("Remediation options: {}", Arrays.stream(remediationOptions)
+            logger.debug("Remediation options: {}", Arrays.stream(remediationOptions)
                     .map(RemediationOption::getName)
                     .collect(Collectors.toList()));
         } else {
-            logger.info("Remediation options unavailable");
+            logger.debug("Remediation options unavailable");
         }
     }
 }
