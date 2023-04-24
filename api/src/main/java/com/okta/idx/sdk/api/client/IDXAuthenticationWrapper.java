4e5b4a99f634ea2bcb2ef0d0ef0acc34177e9b9e
Arvind Krishnakumar <arvind.krishnakumar@okta.com>
2023-04-24 11:14:53 -0500

    cleaned up unused import

--- api/src/main/java/com/okta/idx/sdk/api/client/IDXAuthenticationWrapper.java
+++ api/src/main/java/com/okta/idx/sdk/api/client/IDXAuthenticationWrapper.java
@@ -63,7 +63,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
